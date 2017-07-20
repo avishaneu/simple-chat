2c03402728df344714420bafc959420d735dcb9b
avishaneu <ultimate.theory.of.everything@gmail.com>
2017-07-20 10:58:27 +0300

    simple spring boot application with embedded jetty

---
+++ src/main/java/com/avishaneu/testtasks/simplechat/Application.java
@@ -0,0 +1,16 @@
+package com.avishaneu.testtasks.simplechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by tkalnitskaya on 20.07.2017.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
