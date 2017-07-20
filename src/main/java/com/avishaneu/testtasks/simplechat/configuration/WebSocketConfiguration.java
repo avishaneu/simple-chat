9b5bb3839eba6c2e686187217d684437da327160
avishaneu <ultimate.theory.of.everything@gmail.com>
2017-07-20 11:59:53 +0300

    Added websocket configuration

---
+++ src/main/java/com/avishaneu/testtasks/simplechat/configuration/WebSocketConfiguration.java
@@ -0,0 +1,40 @@
+package com.avishaneu.testtasks.simplechat.configuration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.session.web.socket.config.annotation.AbstractSessionWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+/**
+ * Created by tkalnitskaya on 20.07.2017.
+ */
+@Configuration
+@EnableScheduling
+@EnableWebSocketMessageBroker
+public class WebSocketConfiguration extends AbstractSessionWebSocketMessageBrokerConfigurer {
+
+    private static final Logger log = LoggerFactory.getLogger(WebSocketConfiguration.class);
+
+    @Value("${websocket.endpoint}")
+    private String WEBSOCKET_ENDPOINT;
+
+    @Override
+    public void configureStompEndpoints(StompEndpointRegistry registry) {
+        log.info("Configuring stomp endpoint: " + WEBSOCKET_ENDPOINT);
+        registry.addEndpoint(WEBSOCKET_ENDPOINT)
+                .setAllowedOrigins("*")
+                .withSockJS();
+    }
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        log.info("Configuring message broker...");
+        config.enableSimpleBroker("/queue/", "/topic");
+        config.setApplicationDestinationPrefixes("/app");
+        config.setUserDestinationPrefix("/user");
+    }
+}
