53e179a75c7b540e0788fa8be2468b92231a54c3
avishaneu <ultimate.theory.of.everything@gmail.com>
2017-07-20 21:25:37 +0300

    error handling controller added

--- src/main/java/com/avishaneu/testtasks/simplechat/model/Message.java
+++ src/main/java/com/avishaneu/testtasks/simplechat/model/Message.java
@@ -5,15 +5,15 @@ package com.avishaneu.testtasks.simplechat.model;
  */
 public class Message {
 
-    private String body;
+    private String content;
     private String sender;
 
-    public String getBody() {
-        return body;
+    public String getContent() {
+        return content;
     }
 
-    public void setBody(String body) {
-        this.body = body;
+    public void setContent(String content) {
+        this.content = content;
     }
 
     public String getSender() {
@@ -22,5 +22,31 @@ public class Message {
 
     public void setSender(String sender) {
         this.sender = sender;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "content='" + content + '\'' +
+                ", sender='" + sender + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Message message = (Message) o;
+
+        if (content != null ? !content.equals(message.content) : message.content != null) return false;
+        return sender != null ? sender.equals(message.sender) : message.sender == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = content != null ? content.hashCode() : 0;
+        result = 31 * result + (sender != null ? sender.hashCode() : 0);
+        return result;
     }
 }
